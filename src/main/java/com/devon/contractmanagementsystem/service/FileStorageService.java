76abb561b964f623f757967446b9955e85f60b59
Anuj Sinha <anuj.sinha@devon.nl>
2023-06-09 11:40:30 +0530

    Exception, FileDB, FileDBRepository, FileStorageService, Response, FileController classes created to handle file upload and download

---
+++ src/main/java/com/devon/contractmanagementsystem/service/FileStorageService.java
@@ -0,0 +1,35 @@
+package com.devon.contractmanagementsystem.service;
+
+import java.io.IOException;
+import java.util.stream.Stream;
+
+import com.devon.contractmanagementsystem.model.FileDB;
+import com.devon.contractmanagementsystem.repository.FileDBRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.devon.contractmanagementsystem.model.FileDB;
+import com.devon.contractmanagementsystem.repository.FileDBRepository;
+@Service
+public class FileStorageService {
+
+    @Autowired
+    private FileDBRepository fileDBRepository;
+
+    public FileDB store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+
+        return (com.devon.contractmanagementsystem.model.FileDB) fileDBRepository.save(FileDB);
+    }
+
+    public FileDB getFile(String id) {
+        return (FileDB) fileDBRepository.findById(id).get();
+    }
+
+    public Stream<FileDB> getAllFiles() {
+        return fileDBRepository.findAll().stream();
+    }
+}
