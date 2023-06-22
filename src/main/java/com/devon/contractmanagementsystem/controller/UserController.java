8e3b007060905d1e84d967518119f5fd514d04ee
Anuj Sinha <anuj.sinha@devon.nl>
2023-06-22 15:08:34 +0530

    Firstname mapping is added to the dashboard

--- src/main/java/com/devon/contractmanagementsystem/controller/UserController.java
+++ src/main/java/com/devon/contractmanagementsystem/controller/UserController.java
@@ -1,3 +1,47 @@
+//package com.devon.contractmanagementsystem.controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import com.devon.contractmanagementsystem.model.User;
+//import com.devon.contractmanagementsystem.model.UserLoginRequest;
+//
+//import com.devon.contractmanagementsystem.service.UserService;
+//
+//@RestController
+//@CrossOrigin
+//
+//public class UserController {
+//
+//    @Autowired
+//    UserService userService;
+//
+//
+//    @PostMapping("/signup")
+//    public User signUp(@RequestBody User user) {
+//        // Save user to the database
+//        return userService.signUp(user);
+//    }
+//
+//    @PostMapping("/login")
+//    public User login(@RequestBody UserLoginRequest loginRequest) {
+//        // Retrieve user from the database based on email and password
+//        //return userService.login(user);
+//        return userService.login(loginRequest.getEmail(),loginRequest.getPassword());
+//    }
+//
+//    @GetMapping("/firstname")
+//    public String getUserFirstName(@RequestParam("email") String email){
+//        User user = userService.getUserByEmail(email);
+//        if(user != null){
+//            return user.getFirstName();
+//        }
+//        else{
+//            return "";
+//        }
+//    }
+//}
+
 package com.devon.contractmanagementsystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +71,16 @@ public class UserController {
         // Retrieve user from the database based on email and password
         //return userService.login(user);
         return userService.login(loginRequest.getEmail(),loginRequest.getPassword());
+    }
+
+    @GetMapping("/firstname")
+    public String getUserFirstName(@RequestParam("email") String email){
+        User user = userService.getUserByEmail(email);
+        if(user != null){
+            return user.getFirstName();
+        }
+        else{
+            return "";
+        }
     }
 }
